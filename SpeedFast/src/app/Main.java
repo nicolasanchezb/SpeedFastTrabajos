ff4f50760022809ac64fa64614faa9b9c16e5b89
nicolasanchezb <nicola.sanchezb@duocuc.cl>
2026-08-22 22:12:52 -0400

    Add files via upload

---
+++ SpeedFast/src/app/Main.java
@@ -0,0 +1,65 @@
+package app;
+
+import model.Pedido;
+import model.PedidoComida;
+import model.PedidoEncomienda;
+import model.PedidoExpress;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pedido pedidoComida = new PedidoComida(
+                101,
+                "Avenida Alemania 450",
+                4
+        );
+
+        Pedido pedidoEncomienda = new PedidoEncomienda(
+                102,
+                "Calle Independencia 820",
+                6
+        );
+
+        Pedido pedidoExpress = new PedidoExpress(
+                103,
+                "Avenida Pedro Montt 1200",
+                8
+        );
+
+        Pedido[] pedidos = {
+                pedidoComida,
+                pedidoEncomienda,
+                pedidoExpress
+        };
+
+        System.out.println("==================================");
+        System.out.println("       SISTEMA SPEEDFAST");
+        System.out.println("==================================");
+
+        for (Pedido pedido : pedidos) {
+            pedido.mostrarResumen();
+
+            System.out.println(
+                    "Tiempo estimado de entrega: "
+                            + pedido.calcularTiempoEntrega()
+                            + " minutos"
+            );
+
+            System.out.println("----------------------------------");
+        }
+
+        System.out.println();
+        System.out.println("COMPARACIÓN DE TIEMPOS");
+        System.out.println("----------------------------------");
+
+        for (Pedido pedido : pedidos) {
+            System.out.println(
+                    "Pedido " + pedido.getIdPedido()
+                            + " | " + pedido.obtenerTipoEntrega()
+                            + " | " + pedido.calcularTiempoEntrega()
+                            + " minutos"
+            );
+        }
+    }
+}
